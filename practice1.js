5db45c58c99d0e9293d5d30d6b870d9740231154
Surma Begam <sbtisha2001@gmail.com>
2025-07-28 21:12:28 +0600

    practice the javascript

---
+++ practice1.js
@@ -0,0 +1,44 @@
+//Task1
+var money = 1000;
+var appleprice = 320;
+var orangeprice = 380;
+console.log(appleprice + orangeprice);
+console.log(money - (appleprice + orangeprice));
+console.log(handcase = money - (appleprice + orangeprice));
+console.log(handcase);
+//Task2
+var Mathematics = 75.25;
+var Biology = 65;
+var Chemistry = 80;
+var Physics = 35.45;
+var Bangla = 99.50;
+var totalMarks = Mathematics + Biology + Chemistry + Physics + Bangla;
+console.log(totalMarks);
+console.log(totalMarks / 5);
+console.log( totalMarks .toFixed(2) ); // toFixed(2) ensures two decimal places
+//Average marks
+//Task3
+
+var remainder = (119 % 5);
+console.log(remainder);
+
+var remainder = (125 % 5);
+console.log(remainder);
+
+var remainder = (125/5);
+console.log(remainder);
+
+var remainder =125*5;
+console.log(remainder);
+
+//Task4
+var a = isNaN("I am a string");
+var b = isNaN('100');
+console.log(a);
+console.log(b);
+
+var c = isNaN(50-40);
+console.log(c);
+
+var d = isNaN("iam a string"*50);
+console.log(d);
